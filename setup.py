0682bc2cf1cd541ac6726875bd6186cc2c9ad2b3
ISR <163402495+ISR-AIML@users.noreply.github.com>
2024-08-26 23:07:36 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seisaug',
+    version='1.0',
+    author="ISR-AIML",
+    author_email="isr3aiml@gmail.com",
+    description="A Comprehensive Data Augmentation Python Toolkit for Deep Learning.",
+    url="https://github.com/ISR-AIML/SeisAug",
+    packages=find_packages(),
+    keywords=['Seismology', 'Earthquakes', 'Deep Learning', 'Data Augmentation'],
+    install_requires=['obspy', 'matplotlib', 'scipy~=1.10.0', 'jupyter', 'ipywidgets']
+)
